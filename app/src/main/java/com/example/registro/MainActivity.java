248400c56a868f79c52739b732719d8be0a79831
German Encina <gencina2@gmail.com>
2023-06-19 21:45:15 -0400

    app funcional correccion de mensaje de saludo

--- app/src/main/java/com/example/registro/MainActivity.java
+++ app/src/main/java/com/example/registro/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.registro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
     }
-    
+
+    private void initListeners(){
+
+        EditText nombre = findViewById(R.id.etv_first_name);
+        EditText apellido = findViewById(R.id.etv_last_name);
+        EditText email = findViewById(R.id.etv_email);
+        EditText pass = findViewById(R.id.etv_password);
+
+        Button guardar = findViewById(R.id.btn_save_data);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(nombre.getText().toString().isEmpty() ||
+                   apellido.getText().toString().isEmpty() ||
+                   email.getText().toString().isEmpty() ||
+                   pass.getText().toString().isEmpty()){
+
+                    Toast.makeText(getBaseContext(), "Favor llenar todos los campos", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getBaseContext(), "Helouuuu "+ nombre.getText().toString() + " " +apellido.getText().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+
 }
