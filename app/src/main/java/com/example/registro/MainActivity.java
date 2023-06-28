3f8bcd7baf61e5b4925b5eb7195f5bb81ef6ff73
German Encina <gencina2@gmail.com>
2023-06-27 20:22:52 -0400

    - ViewBinding Ejercicio 10

--- app/src/main/java/com/example/registro/MainActivity.java
+++ app/src/main/java/com/example/registro/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.registro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.example.registro.databinding.ActivityMainBinding;
